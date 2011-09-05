b7bfa6e7b4a66b1717f4dd8250d081ceded603e6
John Pang <johnkichu@gmail.com>
2011-09-05 10:41:47 -0400

    second commit

---
+++ HW01/CSS605Homework01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package css605.homework.pkg01;
+
+/**
+ *
+ * @author JPANG
+ */
+public class CSS605Homework01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
